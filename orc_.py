191e170fbb26150a78f9dd7cca835778173d7c80
jadercerqueira <jader58@live.com>
2023-06-09 17:05:57 -0300

    Getting header data
    Getting header data

--- orc_.py
+++ orc_.py
@@ -13,7 +13,7 @@ import pytesseract
 def convert_pdf_img():
 
     # Caminho para o arquivo PDF
-    pdf_path = "pdfs/p10.pdf"
+    pdf_path = "pdfs/11.pdf"
 
     # Convertendo a primeira página do PDF em imagem
     images = convert_from_path(
@@ -25,11 +25,15 @@ def analise_pagina_1(texto):
 
     padrao_resolucao = r"[Rr][Ee][Ss][Oo][Ll][Uu][ÇCGcçg][Aa][Oo]\s(CONSEPE|CONSU)\s(\d+\s/\s(\d+))"
     padrao_data = r"(?<!\d)\d{1,2}\s?/\s?\d{1,2}\s?/\s?\d{4}(?!\d)"
+    padrao_cabecalho = r"\b(\d{1,2}\s?/\s?\d{1,2}\s?/\s?\d{4})\b\s(.*?)RESOLVE:"
+
 
 
     resultado = re.search(padrao_resolucao, texto)
     resultado_data = re.search(padrao_data, texto)
+    resultado_cabecalho = re.search(padrao_cabecalho, texto, re.DOTALL)
     
+    # Pega os dados referentes a resolução
     if resultado:
         n_Resolucao = resultado.group(2)
         ano = resultado.group(3)
@@ -38,12 +42,20 @@ def analise_pagina_1(texto):
     else:
         n_Resolucao = ano = "####"
 
+    # Pega os dados referentes a data
     if resultado_data:
         data = resultado_data.group()
     else:
         data = "####"
+    
+    # Pega os addos referentes ao cabeçalho
+    if resultado_cabecalho:
+        print(resultado_cabecalho)
+        cabecalho = resultado_cabecalho.group(2)
+    else:
+        cabecalho = "####"
         
-    return n_Resolucao, ano, data
+    return n_Resolucao, ano, data, cabecalho
 
 def analise_reitora(texto):
     padrao_reitor = r"Documento assinado eletronicamente por\s(.*?)(?=,)"
@@ -85,7 +97,8 @@ for i, img in enumerate(data):
 
     # Caso seja a primeira página do documento, pegue o número da resolução e o ano correspondente.
     if i == 0:
-        resol, ano, data = analise_pagina_1(result)
+        resol, ano, data, cabecalho = analise_pagina_1(result)
+        print(result)
     
     # Analisa em alguma página se encontra o formato especifico onde se nomeia o reitor/reitora.
     if not valor_reitor:
@@ -96,6 +109,7 @@ for i, img in enumerate(data):
 print("Resolução: ", resol)
 print("Ano: ", ano)
 print("Data: ", data)
+print("Cabeçalho: ", cabecalho)
 print("Reitor: ", reitor) 
 
     
